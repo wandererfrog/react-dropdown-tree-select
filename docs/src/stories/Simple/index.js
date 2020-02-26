c6c755237372c690ea79fe089a68241bc4e49b88
Diogo Afonso Barroso <diogo.barroso@protonmail.com>
2020-02-26 10:08:58 +0100

    Adds specific callback for reorder list

--- docs/src/stories/Simple/index.js
+++ docs/src/stories/Simple/index.js
@@ -23,6 +23,10 @@ const onBlur = () => {
   console.log('onBlur')
 }
 
+const onReorderList = (newList) => {
+  console.log(newList)
+}
+
 const Simple = () => (
   <div>
     <h1>Basic component</h1>
@@ -43,18 +47,80 @@ const Simple = () => (
       onChange={onChange}
       onAction={onAction}
       onNodeToggle={onNodeToggle}
+      onReorderList={onReorderList}
       onFocus={onFocus}
       onBlur={onBlur}
       sortable
       className="demo"
     />
     <DropdownTreeSelect
-      data={data}
+      data={[
+        {
+          "label": "Everything",
+          "checked": false,
+          "id": 0.9215653828722463
+        },
+        {
+          "value": 0,
+          "label": "Read a printed newspaper",
+          "checked": false,
+          "id": 1.9085662227177476
+        },
+        {
+          "value": 1,
+          "label": "Visited a newspaper website",
+          "checked": false,
+          "id": 2.2386511444784674
+        },
+        {
+          "value": 2,
+          "label": "Read a printed magazine",
+          "checked": false,
+          "id": 3.9683844898405116
+        },
+        {
+          "value": 3,
+          "label": "Read a digital magazine",
+          "checked": false,
+          "id": 4.627763356841399
+        },
+        {
+          "value": 4,
+          "label": "Watched television",
+          "checked": false,
+          "id": 5.743144682851218
+        },
+        {
+          "value": 5,
+          "label": "Watched television online or on demand",
+          "checked": false,
+          "id": 6.884139654358451
+        },
+        {
+          "value": 6,
+          "label": "Read a book",
+          "checked": false,
+          "id": 7.4333807171852255
+        },
+        {
+          "value": 7,
+          "label": "Read an e-book",
+          "checked": false,
+          "id": 8.58625581627021
+        },
+        {
+          "value": 8,
+          "label": "None of the above",
+          "checked": false,
+          "id": 9.163327936921345
+        }
+      ]}
       onChange={onChange}
       onAction={onAction}
       onNodeToggle={onNodeToggle}
       onFocus={onFocus}
       onBlur={onBlur}
+      sortable
       className="demo"
     />
   </div>
